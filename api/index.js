3926b784852660aa100ea6e3509ff5d3aa0e346b
AD <adhish.g704@gmail.com>
2024-02-29 10:43:41 +0530

    Set up user route API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const PORT = 3000;
 
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+app.use("/api/user", userRouter);
