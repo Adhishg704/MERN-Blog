8b6c53981c3d54126632cf53c63e4c785210dd49
AD <adhish.g704@gmail.com>
2024-04-20 11:38:26 +0530

    added show more functionality to posts tab

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,26 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Table } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
-  const [userPosts, setuserPosts] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
-  console.log(userPosts);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(
-          `http://localhost:3000/api/post/getposts?userId=${currentUser._id}`,
-          {
-            method: "GET",
-          }
-        );
+        const res = await fetch(`http://localhost:3000/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
-          setuserPosts(data.posts);
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -31,22 +29,43 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300">
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
-          <Table hoverable className="shadow-md">
+          <Table hoverable className='shadow-md'>
             <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
-              <Table.HeadCell>Post Image</Table.HeadCell>
-              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell>Edit</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body key={post._id}>
-                <Table.Row className="bg-white">
+              <Table.Body className='divide-y' key = {post._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
@@ -55,26 +74,46 @@ export default function DashPosts() {
                       <img
                         src={post.image}
                         alt={post.title}
-                        className="w-12 h-12 object-cover"
+                        className='w-20 h-10 object-cover bg-gray-500'
                       />
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className="font-medium text-gray-900" to={`/post/${post.slug}`}>{post.title}</Link>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className="text-red-500 font-medium cursor-pointer hover:underline">Delete</span>
+                    <span
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                    >
+                      Delete
+                    </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link to={`/update-post/${post._id}`}>
-                      <span className="text-teal-500 hover:underline">Edit</span>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
                     </Link>
                   </Table.Cell>
                 </Table.Row>
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -112,7 +112,7 @@ export default function CreatePost() {
             <option value="python">Python</option>
             <option value="machine-learning">Machine Learning</option>
             <option value="database">Database</option>
-            <option value="database">Cloud</option>
+            <option value="cloud">Cloud</option>
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
